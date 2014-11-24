d05fa5af7b9fe6e44380f131b9b6204c79d96596
Jordan Byrne <jordan5039@live.com>
2014-11-24 20:30:17 +0000

    first commit

--- Calculate Tips/ViewController.swift
+++ Calculate Tips/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mealCostTextField: UITextField!
+    @IBOutlet weak var tipPercentageTextField: UITextField!
+    @IBOutlet weak var ansLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true;
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func CalculateTipButton(sender: AnyObject) {
+        ansLabel.text = "$" + calculateTip()
+        ansLabel.hidden = false
+        mealCostTextField.text = ""
+        tipPercentageTextField.text = ""
     }
-
+    
+    func calculateTip() -> NSString {
+        var mealCost: NSString = mealCostTextField.text
+        let mealCostAsDouble = mealCost.doubleValue
+        var tipPercent: NSString = tipPercentageTextField.text
+        let tipPercentAsDouble = tipPercent.doubleValue / 100
+        
+        var answer = mealCostAsDouble * tipPercentAsDouble
+        return NSString(format: "%0.2f", answer)
+    }
 
 }
 
